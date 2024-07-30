5ba13bb6b260ea491e6ac4e4b53af3f2fcb12e10
Abdul Sameed <160429352+sameedpanjwani19@users.noreply.github.com>
2024-07-30 18:55:35 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,96 +1,183 @@
-"use strict";
+// DOM Elements
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const valueEl = document.querySelector('.value');
 
-var input = document.getElementById('input'), 
-    number = document.querySelectorAll('.numbers div'), 
-    operator = document.querySelectorAll('.operators div'), 
-    result = document.getElementById('result'), 
-    clear = document.getElementById('clear'), 
-    resultDisplayed = false; 
+const acEl = document.querySelector('.ac');
+const pmEl = document.querySelector('.pm');
+const percentEl = document.querySelector('.percent');
 
-for (var i = 0; i < number.length; i++) {
-    number[i].addEventListener("click", function (e) {
+const additionEl = document.querySelector('.addition');
+const subtractionEl = document.querySelector('.subtraction');
+const multiplicationEl = document.querySelector('.multiplication');
+const divisionEl = document.querySelector('.division');
+const equalEl = document.querySelector('.equal');
 
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
+const decimalEl = document.querySelector('.decimal');
+const number0El = document.querySelector('.number-0');
+const number1El = document.querySelector('.number-1');
+const number2El = document.querySelector('.number-2');
+const number3El = document.querySelector('.number-3');
+const number4El = document.querySelector('.number-4');
+const number5El = document.querySelector('.number-5');
+const number6El = document.querySelector('.number-6');
+const number7El = document.querySelector('.number-7');
+const number8El = document.querySelector('.number-8');
+const number9El = document.querySelector('.number-9');
+const numberElArray = [
+  number0El, number1El, number2El, number3El, number4El,
+  number5El, number6El, number7El, number8El, number9El
+];
 
-        if (resultDisplayed === false) {
-            input.innerHTML += e.target.innerHTML;
-        } else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-            resultDisplayed = false;
-            input.innerHTML += e.target.innerHTML;
-        } else {
-            resultDisplayed = false;
-            input.innerHTML = "";
-            input.innerHTML += e.target.innerHTML;
-        }
 
-    });
-}
+// variables
+let valueStrInMemory = null;
+let operatorInMemory = null;
 
-for (var i = 0; i < operator.length; i++) {
-    operator[i].addEventListener("click", function (e) {
 
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length - 1];
+// Functions
+const getValueAsStr = () => valueEl.textContent.split(',').join('');
 
-        if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-            var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
-            input.innerHTML = newString;
-        } else if (currentString.length == 0) {
-            console.log("enter a number first");
-        } else {
-            input.innerHTML += e.target.innerHTML;
-        }
+const getValueAsNum = () => {
+  return parseFloat(getValueAsStr());
+};
 
-    });
-}
+const setStrAsValue = (valueStr) => {
+  if (valueStr[valueStr.length - 1] === '.') {
+    valueEl.textContent += '.';
+    return;
+  }
 
-result.addEventListener("click", function () {
+  const [wholeNumStr, decimalStr] = valueStr.split('.');
+  if (decimalStr) {
+    valueEl.textContent =
+      parseFloat(wholeNumStr).toLocaleString() + '.' + decimalStr;
+  } else {
+    valueEl.textContent = parseFloat(wholeNumStr).toLocaleString();
+  }
+};
 
-    var inputString = input.innerHTML;
+const handleNumberClick = (numStr) => {
+  const currentValueStr = getValueAsStr();
+  if (currentValueStr === '0') {
+    setStrAsValue(numStr);
+  } else {
+    setStrAsValue(currentValueStr + numStr);
+  }
+};
 
-    var numbers = inputString.split(/\+|\-|\×|\÷/g);
+const getResultOfOperationAsStr = () => {
+  const currentValueNum = getValueAsNum();
+  const valueNumInMemory = parseFloat(valueStrInMemory);
+  let newValueNum;
+  if (operatorInMemory === 'addition') {
+    newValueNum = valueNumInMemory + currentValueNum;
+  } else if (operatorInMemory === 'subtraction') {
+    newValueNum = valueNumInMemory - currentValueNum;
+  } else if (operatorInMemory === 'multiplication') {
+    newValueNum = valueNumInMemory * currentValueNum;
+  } else if (operatorInMemory === 'division') {
+    newValueNum = valueNumInMemory / currentValueNum;
+  }
 
-    var operators = inputString.replace(/[0-9]|\./g, "").split("");
+  return newValueNum.toString();
+};
 
-    console.log(inputString);
-    console.log(operators);
-    console.log(numbers);
-    console.log("----------------------------");
+const handleOperatorClick = (operation) => {
+  const currentValueStr = getValueAsStr();
 
-    var divide = operators.indexOf("÷");
-    while (divide != -1) {
-        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
-        operators.splice(divide, 1);
-        divide = operators.indexOf("÷");
-    }
+  if (!valueStrInMemory) {
+    valueStrInMemory = currentValueStr;
+    operatorInMemory = operation;
+    setStrAsValue('0');
+    return;
+  }
+  valueStrInMemory = getResultOfOperationAsStr();
+  operatorInMemory = operation;
+  setStrAsValue('0');
+};
 
-    var multiply = operators.indexOf("×");
-    while (multiply != -1) {
-        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
-        operators.splice(multiply, 1);
-        multiply = operators.indexOf("×");
-    }
 
-    var subtract = operators.indexOf("-");
-    while (subtract != -1) {
-        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
-        operators.splice(subtract, 1);
-        subtract = operators.indexOf("-");
-    }
 
-    var add = operators.indexOf("+");
-    while (add != -1) {
-        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
-        operators.splice(add, 1);
-        add = operators.indexOf("+");
-    }
 
-    input.innerHTML = numbers[0];
+// Add Event Listeners to functions
+acEl.addEventListener('click', () => {
+  setStrAsValue('0');
+  valueStrInMemory = null;
+  operatorInMemory = null;
+});
+pmEl.addEventListener('click', () => {
+  const currentValueNum = getValueAsNum();
+  const currentValueStr = getValueAsStr();
 
-    resultDisplayed = true;
+  if (currentValueStr === '-0') {
+    setStrAsValue('0');
+    return;
+  }
+  if (currentValueNum >= 0) {
+    setStrAsValue('-' + currentValueStr);
+  } else {
+    setStrAsValue(currentValueStr.substring(1));
+  }
+});
+percentEl.addEventListener('click', () => {
+  const currentValueNum = getValueAsNum();
+  const newValueNum = currentValueNum / 100;
+  setStrAsValue(newValueNum.toString());
+  valueStrInMemory = null;
+  operatorInMemory = null;
 });
 
-clear.addEventListener("click", function () {
-    input.innerHTML = "";
-})
+
+// add event listeners to operators
+additionEl.addEventListener('click', () => {
+  handleOperatorClick('addition');
+});
+subtractionEl.addEventListener('click', () => {
+  handleOperatorClick('subtraction');
+});
+multiplicationEl.addEventListener('click', () => {
+  handleOperatorClick('multiplication');
+});
+divisionEl.addEventListener('click', () => {
+  handleOperatorClick('division');
+});
+equalEl.addEventListener('click', () => {
+  if (valueStrInMemory) {
+    setStrAsValue(getResultOfOperationAsStr());
+    valueStrInMemory = null;
+    operatorInMemory = null;
+  }
+});
+
+
+// Add Event Listeners to numbers and decimal
+for (let i=0; i < numberElArray.length; i++) {
+  const numberEl = numberElArray[i];
+  numberEl.addEventListener('click', () => {
+    handleNumberClick(i.toString());
+  });
+}
+decimalEl.addEventListener('click', () => {
+  const currentValueStr = getValueAsStr();
+  if (!currentValueStr.includes('.')) {
+    setStrAsValue(currentValueStr + '.');
+  }
+});
+
+
+// Set up the time
+const updateTime = () => {
+  const currentTime = new Date();
+
+  let currentHour = currentTime.getHours();
+  const currentMinute = currentTime.getMinutes();
+
+  if (currentHour > 12) {
+    currentHour -= 12;
+  }
+  hourEl.textContent = currentHour.toString();
+  minuteEl.textContent = currentMinute.toString().padStart(2, '0');
+}
+setInterval(updateTime, 1000);
+updateTime();
